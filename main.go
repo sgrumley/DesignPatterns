78f453247546e07d6cb694790ba15eeb72a4c234
Sam Grumley <grumsamu@gmail.com>
2020-12-04 04:55:11 -0800

    New TODO

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 // TODO: build images and theory for each concept
 // use this main to interact with each file
-
+// Update readme with general problem and solution info
 package main
 
 func main() {
