92d84be51335a816b3450110fd79519d73044003
Sam Grumley <grumsamu@gmail.com>
2020-12-04 04:53:39 -0800

    added Functional Builder

---
+++ Builder/builderFunctional.go
@@ -0,0 +1,46 @@
+
+// Benefit of this method very easy to extend builder without aggregating nerw builders
+// keeps a list of actions to perform on the object then peroform at once
+package main
+
+import (
+	"fmt"
+)
+
+type Person struct {
+	name, position string
+}
+
+type personMod func(*Person)
+
+type PersonBuilder struct {
+	actions []personMod
+}
+
+func (b *PersonBuilder) Called(name string) *PersonBuilder {
+	b.actions = append(b.actions, func(p *Person){
+		p.name = name
+	})
+	return b
+}
+
+func (b *PersonBuilder) WorksAsA(job string) *PersonBuilder {
+	b.actions = append(b.actions, func(p *Person){
+		p.position = job;
+	})
+	return b
+}
+
+func (b *PersonBuilder) Build() *Person {
+	p := Person{}
+	for _, a := range b.actions {
+		a(&p)
+	}
+	return &p
+}
+
+func main() {
+	b := PersonBuilder{}
+	p := b.Called("Sam").WorksAsA("Developer").Build()
+	fmt.Println(*p)
+}
