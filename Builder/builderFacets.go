9a60f46b99cbd34945ddde7cfee39c184cc81ab1
Sam Grumley <grumsamu@gmail.com>
2020-12-04 04:02:32 -0800

    added multiple builders

---
+++ Builder/builderFacets.go
@@ -0,0 +1,90 @@
+// Multiple builders 
+// e.g aggregating builders under a single builder
+package main
+
+import ( 
+	"fmt"
+)
+
+type Person struct {
+	// address
+	Address, Zip, City string
+	// job
+	CompanyName, Position string
+	Income int
+}
+
+type PersonBuilder struct {
+	person *Person
+}
+
+func (b *PersonBuilder) Lives() *PersonAddressBuilder {
+	return &PersonAddressBuilder{*b}
+}
+
+func (b *PersonBuilder) Works() *PersonJobBuilder {
+	return &PersonJobBuilder{*b}
+}
+
+func newPersonBuilder() *PersonBuilder {
+	return &PersonBuilder{ &Person{} }
+}
+
+type PersonAddressBuilder struct {
+	PersonBuilder
+}
+
+func (it *PersonAddressBuilder) At(address string) *PersonAddressBuilder {
+	it.person.Address = address;
+	return it
+}
+
+func (it *PersonAddressBuilder) In(city string) *PersonAddressBuilder {
+	it.person.City = city;
+	return it
+}
+
+func (it *PersonAddressBuilder) withZip(zip string) *PersonAddressBuilder {
+	it.person.Zip = zip;
+	return it
+}
+
+type PersonJobBuilder struct {
+	PersonBuilder
+}
+
+func (it *PersonJobBuilder) At(companyName string) *PersonJobBuilder {
+	it.person.CompanyName = companyName;
+	return it
+}
+
+func (it *PersonJobBuilder) AsA(position string) *PersonJobBuilder {
+	it.person.Position = position;
+	return it
+}
+
+func (it *PersonJobBuilder) Earning(income int) *PersonJobBuilder {
+	it.person.Income = income;
+	return it
+}
+
+func (b *PersonBuilder) Build() *Person {
+	return b.person
+}
+
+func main() {
+	pb := newPersonBuilder()
+	pb.
+	Lives().
+		At("24 East Street").
+		In("Australia").
+		withZip("4000").
+	Works().
+		At("Google").
+		AsA("Software Developer").
+		Earning(100000)
+
+	person := pb.Build()
+	fmt.Println(person)
+	// fmt.Println("ndf")
+}
